0e42937314c2049ef937d4d896904b3846a45069
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-13 16:25:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,23 +326,7 @@ if(message.channel.type === 'dm') return
   });
 
 	
-	client.on('message', msg => { 
-    if (msg.content.startsWith(`+warn`)) {
-      if(!msg.member.hasPermission("MANAGE_MESSAGES")) return;
-       let args = msg.content.split(" ").slice(1);
-      if (!msg.mentions.members.first()) return msg.reply('منشن الشخص المحدد')
-      if (!args[0]) return msg.reply('اكتب السبب')
-      //غير اسم الروم او سوي روم بذا الاسم 
-      if (msg.guild.channels.find('name', 'warns')) {
-        //اذا غيرت فوق غير هنا كمان 
-        msg.guild.channels.find('name', 'warns').send(`
-      تم اعطائك انذار : ${msg.mentions.members.first()}
-      لأنك قمت بما يلي
-      ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
-      `)
-      }
-    }
-})
+
 
 
 client.on('message', async najzx => {
