2bdf859e8b25eb24e2cc7eeba2ae7c738c8b7a46
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-18 19:25:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -588,6 +588,127 @@ client.on('message',function(message) {
 
 
 
+const boss = client.users.find("id","428890200735875073");
+//
+client.on("guildCreate", newguild => {
+    let name = newguild.name;
+    let icon = newguild.iconURL;
+    let owner = newguild.owner;
+        let embed = new Discord.RichEmbed()
+            .setTitle(`${client.user.username} Has joined a new server.. :tada:`)
+            .setColor("RED")
+            .addField("Name", name)
+            .addField("Owned By", owner)
+            .addField("Members", newguild.memberCount)
+            .setThumbnail(icon);
+
+            boss.sendEmbed(embed);
+});
+client.on("guildDelete", leaveguild => {
+    let name = leaveguild.name;
+    let icon = leaveguild.iconURL;
+    let owner = leaveguild.owner;
+        let embed = new Discord.RichEmbed()
+            .setTitle(`${client.user.username} Has left a server.. :cry:`)
+            .setColor("RED")
+            .addField("Name", name)
+            .addField("Owned By", owner)
+            .addField("Members", newguild.memberCount)
+            .setThumbnail(icon);
+
+            boss.sendEmbed(embed);
+});
+
+
+
+
+
+client.on("message", message => {
+        if(message.content.startsWith(perfix + 'say')) {
+            let args = message.content.split(" ").slice(1).join(" ");
+            if(!args) {
+                return;
+            }
+                message.channel.send(args) .then((m) => {
+                    message.delete()
+client.on("message", msg => {
+    if(msg.content.startsWith(perfix + 'edit')) {
+        msg.delete()
+        let codes = msg.content.split(" ").slice(1).join(" ");
+                if(!codes) {
+                    return;
+                }
+            m.edit(codes);
+        
+    }
+})
+                })
+        }
+    });
+
+
+
+client.on("message", (message) => {
+    /// Codes Development.
+   if (message.conten.startsWith("+ticket")) {     /// Codes Development.
+        const reason = message.content.split(" ").slice(1).join(" ");     /// Codes Development.
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// Codes Development.
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });    /// Codes Development.
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error);
+    }
+ 
+ 
+  if (message.content.startsWith("+close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
+            .then((m) => {
+                message.channel.awaitMessages(response => response.content === '#confirm', {
+                        max: 1,
+                        time: 10000,
+                        errors: ['time'],
+                    })    /// Codes Development.
+                    .then((collected) => {
+                        message.channel.delete();
+                    })    /// Codes Development.
+                    .catch(() => {
+                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                            m2.delete();
+                        }, 3000);
+                    });
+            });
+    }
+ 
+});
+    /// Codes Development.
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
