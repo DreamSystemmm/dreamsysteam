551ddcf4f01d04d3af7ef9f402a1bcf575808860
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-19 07:18:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -676,4 +676,140 @@ client.on('message' , async (message) => {
 
 
 
+client.on("message", async message => {
+        if(!message.channel.guild) return;
+    if(message.content.startsWith(prefix + 'member')) {
+        let guild = await message.guild.fetchMembers()
+        let bots = guild.members.filter(m => m.user.bot).size
+        let members = guild.memberCount
+        let humans = members - bots
+        let dndusers = guild.members.filter(member => member.user.presence.status === "dnd")
+        let awayusers = guild.members.filter(member => member.user.presence.status === "idle")
+        let onlineusers = guild.members.filter(member => member.user.presence.status === "online")
+        let offlineusers = guild.members.filter(member => member.user.presence.status === "offline")
+        var embed = new Discord.RichEmbed()
+              .setColor("#000000")
+              .setTitle("Membercount")
+              .setDescription("Membercount in " + guild.name)
+              .addField("Members", `${members}`, true)
+              .addField("Humans", `${humans}`, true)
+              .addField("Bots", `${bots}`, true)
+              .addField("Status Users", `Online Users: ${onlineusers.size}\nDND Users: ${dndusers.size}\nAway Users: ${awayusers.size}\nOffline Users: ${offlineusers.size}\nTotal Members: ${message.guild.memberCount}`, true)
+              .setThumbnail(message.author.avatarURL)
+              message.channel.send({ embed: embed });
+     
+      }
+     
+        });
+
+
+
+
+client.on("message", async message => {
+        if(!message.channel.guild) return;
+
+        if(message.content.startsWith(prefix + 'server')) {
+        let guild = message.guild
+        let channel = message.channel
+        let guildicon = guild.icon_url
+        let members = guild.memberCount
+        let bots = guild.members.filter(m => m.user.bot).size
+        let humans = members - bots
+        let allchannels = guild.channels.size
+        let textchannels = guild.channels.filter(e => e.type === "text")
+        let voicechannels = guild.channels.filter(e => e.type === "voice")
+          var embed = new Discord.RichEmbed()
+          .setColor("#000000")
+          .setTitle(`معلومات عن السيرفر`)
+          .setDescription(`معلومات عن : ${guild.name}`)
+          .addField("صاحب السيرفر :", `${guild.owner}`, true)
+          .addField("أيدي السيرفر :", `${guild.id}`, true)
+          .addField("موقع السيرفر :", `${guild.region}`, true)
+          .addField("مستوى حماية السيرفر :", `${guild.verificationLevel}`, true)
+          .addField("عدد الرومات الصوتية :", `${voicechannels.size}`, true)
+          .addField("عدد الرومات الكتابية :", `${textchannels.size}`, true)
+          .addField("عدد اعضاء السيرفر :", `${members}`, true)
+          .addField("عدد البوتات :", `${bots}`, true)
+          .addField("عدد الاشخاص :", `${humans}`, true)
+          .addField("عدد رتب السيرفر :", `${guild.roles.size}`, true)
+          .addField(`أيموجيات الخاصة بالسيرفر : (${guild.emojis.size})`, `- ${guild.emojis.array()}`, true)
+          .setFooter(`تم انشاء هذه السيرفر في: ${guild.createdAt}`)
+ 
+       message.channel.send({ embed: embed });
+ 
+      }
+    });
+
+
+
+
+
+client.on('message', message => {
+ 
+           if (message.content === prefix + "date") {
+           if (!message.channel.guild) return message.reply('** This command only for servers **');  
+           var currentTime = new Date(),
+              hours = currentTime.getHours() + 4 ,
+              hours2 = currentTime.getHours() + 3 ,
+              hours3 = currentTime.getHours() + 2 ,
+              hours4 = currentTime.getHours() + 3 ,
+              minutes = currentTime.getMinutes(),
+              seconds = currentTime.getSeconds(),
+              Year = currentTime.getFullYear(),
+              Month = currentTime.getMonth() + 1,
+              Day = currentTime.getDate();
+               var h = hours
+    if(hours > 12) {
+                 hours -= 12;
+              } else if(hours == 0) {
+                  hours = "12";
+              }  
+               if(hours2 > 12) {
+                 hours2 -= 12;
+              } else if(hours2 == 0) {
+                  hours2 = "12";
+               
+              }  
+  if(hours3 > 12) {
+                 hours3 -= 12;
+              } else if(hours3 == 0) {
+                  hours3 = "12";
+              }
+              if (minutes < 10) {
+                  minutes = '0' + minutes;
+              }
+   
+   
+              var suffix = 'صباحاَ';
+              if (hours >= 12) {
+                  suffix = 'مساء';
+                  hours = hours - 12;
+              }
+              if (hours == 0) {
+                  hours = 12;
+              }
+   
+   
+                  var Date15= new Discord.RichEmbed()
+                  .setThumbnail("https://i.imgur.com/ib3n4Hq.png")
+                  .setTitle( "「التاريخ  والوقت」")
+                  .setColor('RANDOM')
+                  .setFooter(message.author.username, message.author.avatarURL)
+                  .addField('الامارات',
+                  "「"+ hours + ":" + minutes +":"+ seconds + "」")
+                   .addField('مكه المكرمه',
+                  "「"+ hours2 + ":" + minutes +":"+ seconds  + "」")
+                  .addField('مصر',
+                  "「"+ hours3 + ":" + minutes +":"+ seconds  + "」")
+                   
+                  .addField('Date',
+                  "「"+ Day + "-" + Month + "-" + Year +  "」")
+   
+                   message.channel.sendEmbed(Date15);
+          }
+      });
+
+
+
+
 client.login(process.env.BOT_TOKEN);
