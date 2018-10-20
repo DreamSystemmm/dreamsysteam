b1c258f1021ab04121e01727eb88e7d83be2ce59
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-20 08:36:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -812,54 +812,37 @@ client.on('message', message => {
 
 
 
-
-           client.on('message', message => {
-                    var prefix = "+";
-
-           if (message.content.startsWith(prefix + "id")) {
-                     if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
-
-                message.guild.fetchInvites().then(invs => {
-      let member = client.guilds.get(message.guild.id).members.get(message.author.id);
-      let personalInvites = invs.filter(i => i.inviter.id === message.author.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-      var moment = require('moment');
-      var args = message.content.split(" ").slice(1);
-let user = message.mentions.users.first();
-var men = message.mentions.users.first();
- var heg;
- if(men) {
-     heg = men
- } else {
-     heg = message.author
- }
-var mentionned = message.mentions.members.first();
-  var h;
- if(mentionned) {
-     h = mentionned
- } else {
-     h = message.member
- }
-        moment.locale('ar-TN');
-      var id = new  Discord.RichEmbed()
-       
-    .setColor("#0a0909")
-    .setAuthor(message.author.username, message.author.avatarURL) 
-.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
-.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-.addField(': عدد الدعوات', inviteCount,false)
-.setFooter("TINEOUT_408")  
-    message.channel.sendEmbed(id);
-})
-}
-    
-
-         
-     });
-
-
-
-
+client.on('message', message => {
+    let log = message.client.channels.find('id', '503093656664801281')
+    let reason = message.content.split(" ").slice(2).join(' ');
+    let p = message.mentions.members.first();
+    if (message.author.bot) return;
+    if(!message.channel.guild) return;
+    if(message.content.startsWith(prefix + "mute")){
+        
+        if (!message.member.roles.find('name', 'Muted')) return message.reply('**هذا الأمر مخصص للادارة فقط !**').then(message => message.delete(3000));
+        if(!p) return message.reply(`**ضع منشن للشخص المراد اسكاته**`).then(message => message.delete(3000));
+        if(reason.length < 1) return message.reply(`**ضع صورة تثبت صحة الاسكات**`).then(message => message.delete(3000));
+        if(!reason.includes("prntscr")) return message.reply(`**يجب ان يكون السبب صورة**`).then(message => message.delete(3000));
+        if(p.roles.find('name', 'Muted')) return message.reply('**تم اسكات هذا العضو بالفعل**').then(message => message.delete(3000));
+            
+        p.addRole(message.guild.roles.find("name", "Muted")).then (message.reply('**Done.**'));
+        
+        let embed = new Discord.RichEmbed()
+        .setTitle(`New Mute!`)
+        .addField(`For`, `<@${p.user.id}>`)
+        .addField(`By`, `<@${message.author.id}>`)
+        .addField(`Reason`, reason)
+        .addField(`In Chat`, `<#${message.channel.id}>`)
+        .setColor("#161414")
+        .setTimestamp()
+        .setFooter(" ")
+        
+        message.delete();
+        
+        log.send({embed})
+    }
+});
 
 
 
