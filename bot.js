804a0e79c2be0827847b0f2d94f183f1965ad44a
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-20 08:49:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -581,30 +581,6 @@ client.on("guildDelete", leaveguild => {
 
 
 
-client.on("message", message => {
-        if(message.content.startsWith(perfix + 'say')) {
-            let args = message.content.split(" ").slice(1).join(" ");
-            if(!args) {
-                return;
-            }
-                message.channel.send(args) .then((m) => {
-                    message.delete()
-client.on("message", msg => {
-    if(msg.content.startsWith(perfix + 'edit')) {
-        msg.delete()
-        let codes = msg.content.split(" ").slice(1).join(" ");
-                if(!codes) {
-                    return;
-                }
-            m.edit(codes);
-        
-    }
-})
-                })
-        }
-    });
-
-
 
 
 client.on('message' , async (message) => {
