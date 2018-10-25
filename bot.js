ad1a9bebfa68a86bc656690d9e6fa26efcb50ee3
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-25 18:28:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -972,8 +972,75 @@ client.on("ready", () => {
   function lol() {
     client.guilds.get('500569873768906752').roles.find("name", "RanBow").setColor("RANDOM");
   };
-  setInterval(lol, 100);
+  setInterval(lol, 10000);
 });
+
+
+client.on('message', msg => {
+ if(msg.content === "+bot") {
+let embed24 = new Discord.RichEmbed()   
+   .setThumbnail(client.user.avatarURL)
+   .setColor("RANDOM")  
+   .setTitle(`🤖**Information about**🤖 || ${client.user.tag}`, true)
+   .addField("📜**Name + Tag**📜", client.user.tag, true)
+   .addField("🤖**Bot Join Servers**🤖", client.guilds.size, true)
+   .addField("👥**Sender**👥", msg.author.tag, true)
+   .addField("🤖:id: *Bot ID** :id:🤖 ", client.user.id, true)
+   .addField("📆**Bot Created At**📆", `${moment(client.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(client.user.createdAt).fromNow()}\``, true)
+   .addField("🤖**User**🤖", client.users.size, true)
+   .setFooter(`${msg.author.tag}`, `${msg.author.avatarURL}`, true)
+msg.channel.sendEmbed(embed24)
+}
+ });
+
+
+
+
+
+client.on("message", msg => {//By DEL.25♥♥#1406 || هاشم || Alpha Codes || All copyrights for me & Alpha Codes.
+var prefix = '+';// البرفكس
+var m = msg.guild.name
+var d = msg.guild.memberCount
+var p = msg.guild.roles.size
+var c = msg.guild.channels.size
+var l = msg.guild.region
+var o = msg.guild.iconURL
+var k = msg.guild.owner
+var i = msg.guild.emojis.size
+var b = msg.guild.members.filter(m => m.user.bot).size
+var h = d - b
+var cre = `${moment(msg.guild.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(msg.guild.createdAt).fromNow()}\``
+var t = msg.guild.channels.filter(e => e.type === "text")
+var v = msg.guild.channels.filter(e => e.type === "voice")
+var pow = msg.guild.verificationLevel
+var e = msg.guild.emojis.size
+var e2 = msg.guild.emojis.array()
+var afk = msg.guild.afkChannel
+var ID = msg.guild.id
+if (msg.content.startsWith(prefix + "معلومات السيرفر")){
+var embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setThumbnail(o)
+.addField("👑**ريْس السيرفر**👑⤵", k, true)
+.addField("📜**اسم السيرفر**📜⤵", m, true)
+.addField("🆔**ايدي السيرفر**🆔⤵", ID, true)
+.addField("👥**كل الاعضاء**🤖⤵", d, true)
+.addField("📕**الرتب**📕⤵", p, true)
+.addField("📕**القنوات**📕⤵", c, true)
+.addField("🌐**نوع السيرفر**🌐⤵", l, true)
+.addField("🤖**البوتات**🤖⤵", b, true)
+.addField("👥**الاشخاص**👥⤵", h, true)
+.addField("📝**الرومات الكتابيه**📝⤵", `${t.size}`, true)
+.addField("🔒**مستوى حماية السيرفر**🔒⤵", pow, true)
+.addField("🎤**الرومات الصوية**🎤⤵", `${v.size}`, true)
+.addField("📆**تم انشاء السيرفر في**📆⤵", cre,true)
+.addField("🛏غرفت النوم🛏⤵", afk, true)
+msg.channel.sendEmbed(embed);
+}
+});//حقوق الفا كودز || هاشم ♥ || Alpha Codes || All copyrights for me & Alpha Codes.
+
+
+
 
 
 
