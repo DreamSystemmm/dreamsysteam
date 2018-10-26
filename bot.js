a454290be3d1bbe2ccb959c0f8ed1eb1003d12e2
DreamSystemmm <44110968+DreamSystemmm@users.noreply.github.com>
2018-10-26 07:15:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1035,8 +1035,6 @@ client.on('message', message => {
   
 });
 
-
-let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
   if (message.author.bot) return;
  if(!message.channel.guild)return;
@@ -1140,9 +1138,6 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 }
  
 });
-
-
-
 
 
 client.login(process.env.BOT_TOKEN);
